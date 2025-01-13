349fbcccd09df859f8eb9965a70829642fcc89e2
REhpoin <gaog9802@gmail.com>
2025-01-13 16:49:35 +0800

    Add files via upload

---
+++ 剑客源码.lua
@@ -0,0 +1,9 @@
+local UILib = loadstring(game:HttpGet('https://pastebin.com/raw/5p69WmCy'))();
+local notifications = loadstring(game:HttpGet(("https://pastebin.com/raw/CcenX7YB"),true))()
+local win = UILib:Window("Doors Destroyer V1",Color3.fromRGB(102, 255, 153), Enum.KeyCode.RightControl)
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+--- Section
+
+local HomeSection = win:Tab("Home")
