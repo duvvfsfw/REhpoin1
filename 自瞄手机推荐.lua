9d26b825e69b85b89d688f99f544f45fde77f903
REhpoin <gaog9802@gmail.com>
2025-01-15 17:36:27 +0800

    Add files via upload

---
+++ 自瞄手机推荐.lua
@@ -0,0 +1,21 @@
+
+getgenv().Camlock_Settings = {
+        Prediction = 0.01,
+        AimPart = "Head",
+        Key = "q",
+        AutoPrediction = false,
+        Notification = false,
+        Button = true,
+        AntiGroundShots = false,
+        UnderGroundResolver = false,
+
+        -- DO NOT TOUCH THIS OR THE CAMLOCK WILL NOT WORK --
+        Version = "2.5.1",
+        Credits = "space_0999",
+        DiscordServer = "discord.gg/SKhamGzTdn"
+    }
+    
+    
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/elxocasXD/Trip-Hub/main/Scripts/Cam%20Lock.lua'))()
+     
+    
